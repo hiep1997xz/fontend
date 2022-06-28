73b3d7019f5fbe74a037f38b38d07dd9f331bf50
hiep1997xz <hiep1997xz@gmail.com>
2022-06-28 16:28:04 +0700

    feature/home-body

---
+++ src/components/CardHomeOffset/CardHomeOffset.jsx
@@ -0,0 +1,43 @@
+import classNames from 'classnames/bind'
+import { Link } from 'react-router-dom'
+import styles from './CardHomeOffset.module.scss'
+import dataCartHome from './dataCartHomeOffset'
+import SelectCard from '../../assets/SelectCard.svg'
+
+const cx = classNames.bind(styles)
+
+const CardHomeOffset = () => {
+  console.log(dataCartHome)
+  return (
+    <>
+      {dataCartHome.map((card, index) => {
+        console.log(card)
+        return (
+          <div className={cx('cart-list')} key={index}>
+            <img src={card.img} alt="" className={cx('card-img')} />
+            <p className={cx('card-name')}>{card.name}</p>
+            <p className={cx('material')}>
+              <span className={cx('text-card')}>Chất liệu: </span>
+              <span className={cx('text-card-material')}>{card.material}</span>
+            </p>
+            <p>
+              <span className={cx('text-card')}>Gia công: </span>
+              <span className={cx('text-card-machining')}>
+                {card.machining}
+              </span>
+            </p>
+            <p className={cx('text-quote')}>
+              <span className={cx('text-card')}>Liên hệ nhận </span>
+              <Link to="#" className={cx("text-card-quote")}>
+                <span>báo giá</span>
+                <img src={SelectCard} alt="" className={cx('icon-SelectCard')}/>
+              </Link>
+            </p>
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+export default CardHomeOffset
---
+++ src/components/CardHomeOffset/dataCartHomeOffset.jsx
@@ -0,0 +1,68 @@
+import img1 from '../../assets/ImgCardHome/img1.jpg'
+import img2 from '../../assets/ImgCardHome/img2.jpg'
+import img3 from '../../assets/ImgCardHome/img3.jpg'
+import img4 from '../../assets/ImgCardHome/img4.jpg'
+import img5 from '../../assets/ImgCardHome/img5.jpg'
+import img6 from '../../assets/ImgCardHome/img6.jpg'
+import img7 from '../../assets/ImgCardHome/img7.jpg'
+import img8 from '../../assets/ImgCardHome/img8.jpg'
+
+/* eslint-disable import/no-anonymous-default-export */
+export default [
+  {
+    id: 1,
+    img: img1,
+    name: 'Name card visit',
+    material: 'Giấy C300, ánh trai, giấy mỹ thuật, nhựa PCV.',
+    machining: 'Cán mờ, cán bóng...',
+  },
+  {
+    id: 2,
+    img: img2,
+    name: 'Giấy mời, thiệp mời',
+    material: 'Giấy C300, ánh trai, giấy mỹ thuật, nhựa PCV.',
+    machining: 'Cán mờ, cán bóng...',
+  },
+  {
+    id: 3,
+    img: img3,
+    name: 'Tờ rơi tờ gấp',
+    material: 'Giấy C300, ánh trai, giấy mỹ thuật, nhựa PCV.',
+    machining: 'Cán mờ, cán bóng...',
+  },
+  {
+    id: 4,
+    img: img4,
+    name: 'Catalogue',
+    material: 'Giấy C300, ánh trai, giấy mỹ thuật, nhựa PCV.',
+    machining: 'Cán mờ, cán bóng...',
+  },
+  {
+    id: 5,
+    img: img5,
+    name: 'Voucher',
+    material: 'Giấy C300, ánh trai, giấy mỹ thuật, nhựa PCV.',
+    machining: 'Cán mờ, cán bóng...',
+  },
+  {
+    id: 6,
+    img: img6,
+    name: 'Tem dán, nhãn decal',
+    material: 'Giấy C300, ánh trai, giấy mỹ thuật, nhựa PCV.',
+    machining: 'Cán mờ, cán bóng...',
+  },
+  {
+    id: 7,
+    img: img7,
+    name: 'Menu, thực đơn',
+    material: 'Giấy C300, ánh trai, giấy mỹ thuật, nhựa PCV.',
+    machining: 'Cán mờ, cán bóng...',
+  },
+  {
+    id: 8,
+    img: img8,
+    name: 'Phong bì, tiêu đề thư',
+    material: 'Giấy C300, ánh trai, giấy mỹ thuật, nhựa PCV.',
+    machining: 'Cán mờ, cán bóng...',
+  },
+]
