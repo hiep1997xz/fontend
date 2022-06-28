ae1c66f4570bc7704bad853aff0ce6c81a5202f2
hiep1997xz <hiep1997xz@gmail.com>
2022-06-28 11:52:13 +0700

    feature/slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './layouts/Header/Header'
+import SlideShow from './layouts/Slides/SlideShow'
 
 function App() {
   return (
     <>
       <Router>
         <Header />
+        <SlideShow />
         {/* <Routes>
           <Route path="/" component={Header} />
           <Route path="/login" component={Login} />
