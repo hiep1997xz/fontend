3c3d27c650bf4d6e18a5e683203369132e055127
hiep1997xz <hiep1997xz@gmail.com>
2022-11-01 19:18:28 +0700

    update/detail/part2

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -76,49 +76,89 @@ const Details = () => {
               </p>
             </div>
           </div>
-          <div>
-            <p>Chi tiết sản phẩm</p>
+          <div className={cx('group__text__detail')}>
+            <div className={cx('detail')}>Chi tiết sản phẩm</div>
             <p>In Hộp Trung Thu 4 bánh Kinh Đô</p>
-            <div className={cx('form__text')}></div>
-            <p>Giới thiệu thiết kế hộp bánh</p>
-            <span>
-              Hộp bánh Trung Thu Nguyệt Quang Bảo Hợp 4 bánh là loại hộp nắp âm
-              dương với tông màu chủ đạo là vàng đồng. Đây là một tông màu vô
-              cùng sang trọng và quý phải, là biểu tượng cho tầng lớp quý tộc
-              ngày xưa. Những hộp con đựng bánh được làm từ giấy Ivory 300gsm.
-              Phương pháp in là in offset 4 màu CMYK mặt ngoài, cán mờ, ép kim
-              logo. Hộp lớn bên ngoài được làm từ carton lạnh 2mm bồi giấy C150
-              in màu cán mờ.
-            </span>
+
+            <div className={cx('form__text')}>
+              <div className={cx('form__text__group')}>
+                <p className={cx('specifications')}>Quy cách in</p>
+                <div className={cx('d_flex')}>
+                  <div className={cx('group__title')}>
+                    <p>Kích thước: </p>
+                    <p className={cx('quality')}>Chất lượng: </p>
+                    <p>Phương pháp in: </p>
+                    <p>Gia công: </p>
+                    <p>Kiểu dáng hộp: </p>
+                    <p>Số lượng nhận in: </p>
+                    <p>Báo giá chi tiết: </p>
+                  </div>
+                  <div className={cx('group__desc')}>
+                    <p>Tuỳ chỉnh theo yêu cầu</p>
+                    <div className={cx('quality')}>
+                      <p>Giấy Couche 150 (C150),</p>
+                      <p>Giấy Ivory</p>
+                      <p>Carton lạnh</p>
+                    </div>
+                    <p>In offset 4 màu 1 mặt</p>
+                    <p>Ép nhũ, ép kim logo, phủ UV chi tiết hộp,...</p>
+                    <p>Hộp nắp âm dương</p>
+                    <p>Từ 300 hộp trở lên</p>
+                    <p>Liên hệ trực tiếp đến Trần Gia</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <p>Giới thiệu thiết kế hộp bánh</p>
+              <span className={cx('mgbt30px')}>
+                Hộp bánh Trung Thu Nguyệt Quang Bảo Hợp 4 bánh là loại hộp nắp
+                âm dương với tông màu chủ đạo là vàng đồng. Đây là một tông màu
+                vô cùng sang trọng và quý phải, là biểu tượng cho tầng lớp quý
+                tộc ngày xưa. Những hộp con đựng bánh được làm từ giấy Ivory
+                300gsm. Phương pháp in là in offset 4 màu CMYK mặt ngoài, cán
+                mờ, ép kim logo. Hộp lớn bên ngoài được làm từ carton lạnh 2mm
+                bồi giấy C150 in màu cán mờ.
+              </span>
+            </div>
+
             <div className={cx('form__text__1')}></div>
-            <p>
-              Ưu điểm của mẫu hộp bánh Trung Thu Nguyệt Quang Bảo Hợp 4 bánh
-            </p>
-            <span>
-              Màu vàng đồng nổi bật làm nên sự sang trọng, quý phái cho hộp bánh
-              Sử dụng chất liệu giấy cao cấp, thân thiện với môi trường và an
-              toàn để đựng thực phẩm. Thiết kế độc đáo, ấn tượng đẳng cấp nên
-              rất phù hợp cho việc làm quà tặng cho người thân, bạn bè, đồng
-              nghiệp, đối tác hay nhân viên
-            </span>
-            <p>
-              Trần Gia - Sự lựa chọn hoàn hảo cho thiết kế và in ấn hộp bánh
-              Trung Thu
-            </p>
-            <span>
-              Khi lựa chọn đơn vị thiết kế và in hộp bánh trung thu, khách hàng
-              cần cẩn trọng, xem xét kỹ càng lựa chọn đơn vị in ấn chuyên
-              nghiệp, uy tín. Tránh tình trạng mất thời gian cũng như chi phí
-              tại đơn vị in hộp bánh trung thu giá rẻ. Bởi trên thị trường hiện
-              nay có nhiều cơ sở in ấn không đảm bảo chất lượng sản phẩm và đáp
-              ứng yêu cầu thiết kế, in ấn của khách hàng. Trần Gia - Công ty
-              chuyên thiết kế và in ấn vỏ hộp bánh trung thu đẹp, sáng tạo và uy
-              tín số 1 Việt Nam. Với ưu điểm về kho mẫu hộp bánh trung thu có
-              sẵn, cũng như quy trình thiết kế và in ấn đồng bộ, giúp khách hàng
-              tiết kiệm thời gian và chi phí in hộp bánh trung thu. Bên cạnh đó,
-              giá thành in ấn tại Printgo cạnh tranh nhất trên thị trường, cùng
-              hàng ngàn ưu đãi, khuyến mãi khủng trong mùa Trung thu năm nay.
-            </span>
+
+            <div>
+              <p>
+                Ưu điểm của mẫu hộp bánh Trung Thu Nguyệt Quang Bảo Hợp 4 bánh
+              </p>
+              <span>
+                Màu vàng đồng nổi bật làm nên sự sang trọng, quý phái cho hộp
+                bánh Sử dụng chất liệu giấy cao cấp, thân thiện với môi trường
+                và an toàn để đựng thực phẩm. Thiết kế độc đáo, ấn tượng đẳng
+                cấp nên rất phù hợp cho việc làm quà tặng cho người thân, bạn
+                bè, đồng nghiệp, đối tác hay nhân viên
+              </span>
+            </div>
+            <div>
+              <p className={cx('text_tran')}>
+                Trần Gia - Sự lựa chọn hoàn hảo cho thiết kế và in ấn hộp bánh
+                Trung Thu
+              </p>
+              <span>
+                Khi lựa chọn đơn vị thiết kế và in hộp bánh trung thu, khách
+                hàng cần cẩn trọng, xem xét kỹ càng lựa chọn đơn vị in ấn chuyên
+                nghiệp, uy tín. Tránh tình trạng mất thời gian cũng như chi phí
+                tại đơn vị in hộp bánh trung thu giá rẻ. Bởi trên thị trường
+                hiện nay có nhiều cơ sở in ấn không đảm bảo chất lượng sản phẩm
+                và đáp ứng yêu cầu thiết kế, in ấn của khách hàng. Trần Gia -
+                Công ty chuyên thiết kế và in ấn vỏ hộp bánh trung thu đẹp, sáng
+                tạo và uy tín số 1 Việt Nam. Với ưu điểm về kho mẫu hộp bánh
+                trung thu có sẵn, cũng như quy trình thiết kế và in ấn đồng bộ,
+                giúp khách hàng tiết kiệm thời gian và chi phí in hộp bánh trung
+                thu. Bên cạnh đó, giá thành in ấn tại Printgo cạnh tranh nhất
+                trên thị trường, cùng hàng ngàn ưu đãi, khuyến mãi khủng trong
+                mùa Trung thu năm nay.
+              </span>
+            </div>
+
             <div className={cx('center')}>
               <button className={cx('btn_1')}>
                 <span>YÊU CẦU BÁO GIÁ</span>
